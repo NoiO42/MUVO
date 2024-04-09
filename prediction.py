769f961f0f9ae2782f5b9ed18e1144184e4eba12
TmacAaron <83484024+TmacAaron@users.noreply.github.com>
2024-04-09 12:08:37 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,28 +1,60 @@
+import git
 import os
 import socket
 import time
-from tqdm import tqdm
+from weakref import proxy
 
 import torch
-from torch.utils.tensorboard.writer import SummaryWriter
-# import lightning.pytorch as pl
-import numpy as np
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.callbacks import ModelSummary, LearningRateMonitor
 
 from muvo.config import get_parser, get_cfg
 from muvo.data.dataset import DataModule
 from muvo.trainer import WorldModelTrainer
-from lightning.pytorch.callbacks import ModelSummary
 
 from clearml import Task, Dataset, Model
+
+
+class SaveGitDiffHashCallback(pl.Callback):
+    def setup(self, trainer, pl_model, stage):
+        repo = git.Repo()
+        trainer.git_hash = repo.head.object.hexsha
+        trainer.git_diff = repo.git.diff(repo.head.commit.tree)
+
+    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+        checkpoint['world_size'] = trainer.world_size
+        checkpoint['git_hash'] = trainer.git_hash
+        checkpoint['git_diff'] = trainer.git_diff
+
+
+class MyModelCheckpoint(ModelCheckpoint):
+    def _save_checkpoint(self, trainer: "pl.Trainer", filepath: str) -> None:
+        filename = filepath.split('/')[-1]
+        _checkpoint = trainer._checkpoint_connector.dump_checkpoint(self.save_weights_only)
+        try:
+            torch.save(_checkpoint, filename)
+        except AttributeError as err:
+            key = "hyper_parameters"
+            _checkpoint.pop(key, None)
+            print(f"Warning, `{key}` dropped from checkpoint. An attribute is not picklable: {err}")
+            torch.save(_checkpoint, filename)
+
+        self._last_global_step_saved = trainer.global_step
+
+        # notify loggers
+        if trainer.is_global_zero:
+            for logger in trainer.loggers:
+                logger.after_save_checkpoint(proxy(self))
 
 
 def main():
     args = get_parser().parse_args()
     cfg = get_cfg(args)
 
-    task = Task.init(project_name=cfg.CML_PROJECT, task_name=cfg.CML_TASK, task_type=cfg.CML_TYPE, tags=cfg.TAG)
-    task.connect(cfg)
-    cml_logger = task.get_logger()
+    # task = Task.init(project_name=cfg.CML_PROJECT, task_name=cfg.CML_TASK, task_type=cfg.CML_TYPE, tags=cfg.TAG)
+    # task.connect(cfg)
+    # cml_logger = task.get_logger()
     #
     # dataset_root = Dataset.get(dataset_project=cfg.CML_PROJECT,
     #                            dataset_name=cfg.CML_DATASET,
@@ -30,72 +62,61 @@ def main():
 
     # data = DataModule(cfg, dataset_root=dataset_root)
     data = DataModule(cfg)
-    data.setup()
 
     input_model = Model(model_id='').get_local_copy() if cfg.PRETRAINED.CML_MODEL else None
+    # input_model = cfg.PRETRAINED.PATH
     model = WorldModelTrainer(cfg.convert_to_dict(), pretrained_path=input_model)
+    # model = WorldModelTrainer.load_from_checkpoint(checkpoint_path=input_model)
     # model.get_cml_logger(cml_logger)
 
     save_dir = os.path.join(
         cfg.LOG_DIR, time.strftime('%d%B%Yat%H:%M:%S%Z') + '_' + socket.gethostname() + '_' + cfg.TAG
     )
-    # writer = SummaryWriter(log_dir=save_dir)
+    logger = pl.loggers.TensorBoardLogger(save_dir=save_dir)
 
-    dataloader = data.test_dataloader()[2]
+    callbacks = [
+        ModelSummary(),
+        SaveGitDiffHashCallback(),
+        LearningRateMonitor(),
+        MyModelCheckpoint(
+            save_dir, every_n_train_steps=cfg.VAL_CHECK_INTERVAL,
+        ),
+    ]
 
-    pbar = tqdm(total=len(dataloader),  desc='Prediction')
-    model.cuda()
+    if cfg.LIMIT_VAL_BATCHES in [0, 1]:
+        limit_val_batches = float(cfg.LIMIT_VAL_BATCHES)
+    else:
+        limit_val_batches = cfg.LIMIT_VAL_BATCHES
 
-    model.train()
-    for module in model.modules():
-        if isinstance(module, torch.nn.Dropout):
-            module.eval()
+    replace_sampler_ddp = not cfg.SAMPLER.ENABLED
 
-    n_prediction_samples = model.cfg.PREDICTION.N_SAMPLES
+    trainer = pl.Trainer(
+        # devices=cfg.GPUS,
+        accelerator='auto',
+        # strategy='ddp',
+        precision=cfg.PRECISION,
+        # sync_batchnorm=True,
+        max_epochs=None,
+        max_steps=cfg.STEPS,
+        callbacks=callbacks,
+        logger=logger,
+        log_every_n_steps=cfg.LOGGING_INTERVAL,
+        val_check_interval=cfg.VAL_CHECK_INTERVAL * cfg.OPTIMIZER.ACCUMULATE_GRAD_BATCHES,
+        check_val_every_n_epoch=None,
+        # limit_val_batches=limit_val_batches,
+        limit_val_batches=3,
+        # use_distributed_sampler=replace_sampler_ddp,
+        accumulate_grad_batches=cfg.OPTIMIZER.ACCUMULATE_GRAD_BATCHES,
+        num_sanity_val_steps=0,
+        profiler='simple',
+    )
 
-    for i, batch in enumerate(dataloader):
-        output_imagines = []
-        ims = []
-        batch = {key: value.cuda() for key, value in batch.items()}
-        with torch.no_grad():
-            batch = model.preprocess(batch)
-            batch_rf = {key: value[:, :model.rf] for key, value in batch.items()}  # dim (b, s, 512)
-            # batch_fh = {key: value[:, model.rf:] for key, value in batch.items()}  # dim (b, s, 512)
-            output, state_dict = model.model.forward(batch_rf, deployment=False)
+    # trainer.fit(model, datamodule=data)
+    trainer.test(model, dataloaders=data.test_dataloader())
 
-            state_imagine = {'hidden_state': state_dict['posterior']['hidden_state'][:, -1],
-                             'sample': state_dict['posterior']['sample'][:, -1],
-                             'throttle_brake': batch['throttle_brake'][:, model.rf:],
-                             'steering': batch['steering'][:, model.rf:]}
-            for _ in range(n_prediction_samples):
-                output_imagine = model.model.imagine(state_imagine, predict_action=False, future_horizon=model.fh)
-                output_imagines.append(output_imagine)
-                voxel_im = torch.where(torch.argmax(output_imagine['voxel_1'].detach(), dim=-4).cpu() != 0)
-                voxel_im = torch.stack(voxel_im).transpose(0, 1).numpy()
-                ims.append({'rgb_re': output_imagine['rgb_1'].detach().cpu().numpy(),
-                            'pcd_re': output_imagine['lidar_reconstruction_1'].detach().cpu().numpy(),
-                            'voxel_re': voxel_im,
-                            })
 
-            model.visualise(batch, output, output_imagines, batch_idx=i, prefix='pred', writer=writer)
-            voxel_label = torch.where(batch['voxel_label_1'].cpu() != 0)
-            voxel_label = torch.stack(voxel_label).transpose(0, 1).numpy()
-            voxel_re = torch.where(torch.argmax(output['voxel_1'].detach(), dim=-4).cpu() != 0)
-            voxel_re = torch.stack(voxel_re).transpose(0, 1).numpy()
-            gt = {'rgb_label': batch['rgb_label_1'].cpu().numpy(),
-                  'throttle_brake': batch['throttle_brake'].cpu().numpy(),
-                  'steering': batch['steering'].cpu().numpy(),
-                  'pcd_label': batch['range_view_label_1'].cpu().numpy(),
-                  'voxel_label': voxel_label,
-                  }
-
-            re = {'rgb_re': output['rgb_1'].detach().cpu().numpy(),
-                  'pcd_re': output['lidar_reconstruction_1'].detach().cpu().numpy(),
-                  'voxel_re': voxel_re,
-                  }
-            upload_data = {'gt': gt, 're': re, 'ims': ims}
-            task.upload_artifact(f'data_{i}', np.array(upload_data))
-        pbar.update(1)
+if __name__ == '__main__':
+    main()
             
 
 if __name__ == '__main__':
